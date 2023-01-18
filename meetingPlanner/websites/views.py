e22750d00f1fac2252e09e158577f92bf7969d99
shejaemeric2 <shejaemeric2@gmail.com>
2023-01-18 11:17:21 +0200

    add models

--- meetingPlanner/websites/views.py
+++ meetingPlanner/websites/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def welcome(request):
+    """greeting view function"""
+
+    return HttpResponse("<h1>This is a greeting from django</h1>")
